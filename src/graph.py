58dd5b4ece4f3c2df34b9c7de71e00298b32e5d1
Artamta <artamta47@gmail.com>
2026-03-16 10:22:04 +0530

    Sync graph.py with main version

--- src/graph.py
+++ src/graph.py
@@ -1,38 +1,5 @@
 import sys
 import json
-<<<<<<< HEAD
-import re
-import networkx as nx
-
-# -----------------------
-# LOAD DATA
-# -----------------------
-
-with open("data/processed/courses.json") as f:
-    courses = json.load(f)
-
-G = nx.DiGraph()
-
-# -----------------------
-# ADD NODES
-# -----------------------
-
-for c in courses:
-
-    code = c.get("course_code")
-
-    if not code:
-        continue
-
-    dept = code[:2]
-
-    G.add_node(
-        code,
-        department=dept,
-        title=c.get("title"),
-        credits=c.get("credits"),
-        semester=c.get("semester")
-=======
 import streamlit as st
 from yfiles_graphs_for_streamlit import StreamlitGraphWidget, Node, Edge, Layout
 
@@ -95,62 +62,11 @@ def make_graph(courses):
         node_label_mapping="label",
         node_color_mapping="color",
         node_parent_mapping="parent_id"
->>>>>>> 01ebc13e4952abdc3defb3eb4aa1eb51227a0d1c
     )
     
     # Call show() with the hierarchic layout constant
     widget.show(graph_layout=Layout.HIERARCHIC)
 
-<<<<<<< HEAD
-
-# -----------------------
-# ADD EDGES
-# -----------------------
-
-for c in courses:
-
-    course_code = c.get("course_code")
-    prereq_text = c.get("prerequisites")
-
-    if not prereq_text:
-        continue
-
-    prereqs = re.findall(r"[A-Z]{2}\d{4}", prereq_text)
-
-    for p in prereqs:
-
-        if p in G.nodes:
-            G.add_edge(p, course_code)
-
-
-print("\nTotal Courses:", G.number_of_nodes())
-print("Total Prerequisite Links:", G.number_of_edges())
-
-
-# -----------------------
-# PRINT DEPARTMENT GRAPHS
-# -----------------------
-
-departments = sorted(set(nx.get_node_attributes(G,"department").values()))
-
-for dept in departments:
-
-    print("\n============================")
-    print("Department:", dept)
-    print("============================")
-
-    dept_nodes = [n for n in G.nodes if G.nodes[n]["department"] == dept]
-
-    for node in dept_nodes:
-
-        children = list(G.successors(node))
-
-        if children:
-            for c in children:
-                print(f"{node}  --->  {c}")
-        else:
-            print(f"{node}")
-=======
 if __name__ == "__main__":
 
     if len(sys.argv) > 1:
@@ -164,4 +80,3 @@ if __name__ == "__main__":
         courses = json.load(file)
 
     make_graph(courses)
->>>>>>> 01ebc13e4952abdc3defb3eb4aa1eb51227a0d1c
