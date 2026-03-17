85018aa946b7fba6728376e04c2e910ecd63b1ee
Artamta <artamta47@gmail.com>
2026-03-18 04:10:03 +0530

    added-yearly-clustering

--- src/graph_algo.py
+++ src/graph_algo.py
@@ -2,6 +2,7 @@
 Custom graph-algorithm for curriculam graph's major minor logic
 
 @author: Artamta
+
 """
 import numpy as np 
 import pandas as pd 
@@ -17,6 +18,11 @@ class File:
 
     def make_list(self):
         i = 0
+        year_1 = []
+        year_2 = []
+        year_3 = []
+        year_4 = []
+
         with open(self.input_file, 'r') as file:
             self.data = json.load(file)
             data = self.data
@@ -28,12 +34,27 @@ class File:
                 sem =course["semesters"]
                 kind =course["kind"]
                 ali=course["aliases"]
+                name = course["course_name"]
+
+                if sem == [1] or sem == [2]:
+                    year_1.append([sem,code,name,credit,kind])
+                elif sem == [3] or sem == [4]:
+                    year_2.append([sem,code,name,credit,kind])
+                elif sem == [5] or sem == [6]:
+                    year_3.append([sem,code,name,credit,kind])
+                elif sem == [7] or sem == [8]:
+                    year_4.append([sem,code,name,credit,kind])
+                else:
+                    print("Error")
+            year_1.sort(),year_2.sort(), year_3.sort(), year_4.sort()
+        return year_1,year_2, year_3, year_4
+           
 
         
 #make-graph
 class Graph:
 
-    def init(self):
+    def __init__(self):
         self.name = name
 
     def graph_init(self):
@@ -53,7 +74,7 @@ class Graph:
 
 #plotting
 class Plot:
-    def init(self):
+    def __init__(self):
         self.name = name
 
     def plot():
@@ -63,10 +84,12 @@ class Plot:
         plt.title("Simple Network Graph using Python")
         plt.show()
 
+
 def main():
     input_file = "../data/IISER-P/all_courses.json"
     content_ = File(input_file)
     content = content_.make_list()
+    print(content)
 
    # print("here is content \n",content)
 if __name__ == "__main__":
